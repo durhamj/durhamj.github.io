981b9e1a21c95fef825edea69b44a9865cca7195
John <durhamj@users.noreply.github.com>
2023-10-02 08:57:52 -0700

    repo reset

--- php-js/it-426/demos/modal-demo/php/test.php
+++
@@ -1,7 +0,0 @@
- <?php 
- echo '<h2>Welcome'. ',' . ' ';
- echo $_POST["username"] . '!' . '</h2>';
- echo '<p>A confirmation email has been sent to' . ' ';
- echo $_POST["email"].'</p>';            
-?>
-  
--- php-js/it-426/demos/php-ajax-demo/php/myparty.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-print '<p class="lyrics">It&#8217;s my party and I&#8217;ll cry if I want to.</p>'
-
-?>
